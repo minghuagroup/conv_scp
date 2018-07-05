5957f95af8245405da89c0834b0a34bd75595ce6
Haiyang Yu <haiyangy@cheyenne4.ib0.cheyenne.ucar.edu>
2018-07-05 15:09:53 -0600

    add linkmymod

---
+++ script/link_mymods_forGCM.sh
@@ -0,0 +1,23 @@
+
+#export ecp_src=/glade/u/home/haiyangy/ECP/src
+export ecp_src=/glade/u/home/mzhang/minghua/conv_scp/src
+
+ln -s $ecp_src/buoysort.F90  ./mymods/
+ln -s $ecp_src/mymods/cam_diagnostics.F90  ./mymods/
+ln -s $ecp_src/mymods/cldwat2m_macro.F90  ./mymods/
+ln -s $ecp_src/mymods/convect_deep.F90  ./mymods/
+ln -s $ecp_src/mymods/convect_shallow.F90  ./mymods/
+ln -s $ecp_src/mymods/conv_intr_jp.F90  ./mymods/
+ln -s $ecp_src/conv_jp.F90  ./mymods/
+ln -s $ecp_src/mymods/micro_mg1_0.F90  ./mymods/
+ln -s $ecp_src/nnparameter.F90  ./mymods/
+ln -s $ecp_src/mymods/phys_control.F90  ./mymods/
+ln -s $ecp_src/mymods/physics_types.F90  ./mymods/
+ln -s $ecp_src/mymods/physpkg.F90  ./mymods/
+ln -s $ecp_src/mymods/runtime_opts.F90  ./mymods/
+ln -s $ecp_src/mymods/uwshcu.F90  ./mymods/
+ln -s $ecp_src/mymods/vertical_diffusion.F90  ./mymods/
+ln -s $ecp_src/mymods/zm_conv.F90  ./mymods/
+ln -s $ecp_src/mymods/zm_conv_intr.F90  ./mymods/
+
+
